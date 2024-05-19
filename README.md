0448fb046bd9d96089c0e2643d19a079a4badf7b
Joao rodrigues <joaopprodrigues08@ua.pt>
2024-05-19 21:59:00 +0100

    predit music working

--- README.md
+++ README.md
@@ -2,3 +2,15 @@
 sudo apt update
 sudo apt install sox
 sudo apt install sox libsox-fmt-all
+pip install zstandard
+
+usage  
+go to src directory  
+python3 run_seg.py seg   
+python3 run_seg.py sig  
+python3 run_seg.py compress  
+python3 run_seg.py pred  
+
+python3 run_seg.py *command* -h  for more detail  
+also  
+python3 run_seg.py clean to clean all files created during the execution
--- src/run_seg.py
+++ src/run_seg.py
@@ -1,10 +1,12 @@
 import os
 import subprocess
 import sys
+import shutil
 import argparse
 import zipfile
+import zstandard as zstd
 from pathlib import Path
-def extract_segment(input_dir, output_dir, start_time, duration, output_format=None, quiet=False):
+def extract_segment(start_time, duration,input_dir="../complete_musics", output_format=None, quiet=False,output_dir="../segment_of_music"):
     # Create the output directory if it doesn't exist
     os.makedirs(output_dir, exist_ok=True)
 
@@ -34,15 +36,14 @@ def extract_segment(input_dir, output_dir, start_time, duration, output_format=N
                 if not quiet:
                     print(f"Error processing {file_name}: {e}")
 
-def create_signature(output_dir,input_dir):
-    command = [
-        'g++', '-W', '-Wall', '-std=c++11', '-o', 'GetMaxFreqs',
-        '../GetMaxFreqs/src/GetMaxFreqs.cpp', '-lsndfile', '-lfftw3', '-lm'
-    ]
-    try:
+def create_signature(output_dir="../signature",input_dir="../complete_musics"):
+    if not os.path.exists('../src/GetMaxFreqs.cpp'):
+        command = [
+            'g++', '-W', '-Wall', '-std=c++11', '-o', 'GetMaxFreqs',
+            '../GetMaxFreqs/src/GetMaxFreqs.cpp', '-lsndfile', '-lfftw3', '-lm'
+        ]
         subprocess.run(command, check=True)
-    except subprocess.CalledProcessError as e:
-        print(f"Error compiling GetMaxFreqs.cpp: {e}")
+  
         
     os.makedirs(output_dir, exist_ok=True)
     for file_name in os.listdir(input_dir):
@@ -60,8 +61,18 @@ def create_signature(output_dir,input_dir):
 def zip_file(file, output_dir, compression):
     if compression == "gzip":
         compression = zipfile.ZIP_DEFLATED
-    else:
+    elif compression == "bzip2":
+        compression= zipfile.ZIP_BZIP2
+    elif compression == "lzma":
+        compression= zipfile.ZIP_LZMA
+    elif compression == "zstd":
+        with open(file, 'rb') as f_in:
+            with open(os.path.join(output_dir, os.path.basename(file) + '.zst'), 'wb') as f_out:
+                cctx = zstd.ZstdCompressor()
+                f_out.write(cctx.compress(f_in.read()))
         return
+    
+    
     file_name_with_extension = os.path.basename(file)
     file_name = os.path.splitext(file_name_with_extension)[0]
     with zipfile.ZipFile(output_dir+'/'+file_name+".zip", 'w', compression) as zipf:
@@ -92,8 +103,7 @@ def append_files(file, path_files, output_dir):
             f.write(initial_content)
             f.write(additional_content)
 
-        print(f"Successfully appended {file_name} to {output_file_path}")
-def compress_data_base(complete_musics,output_compress,compression):
+def compress_data_base(compression,complete_musics ="../signature",output_compress="../compressed"):
     os.makedirs(output_compress, exist_ok=True)
     for file_name in os.listdir(complete_musics):
         file_path = os.path.join(complete_musics ,file_name)
@@ -109,20 +119,12 @@ def delete_files_with_extension(directory, extension):
         # Check if the file ends with the specified extension
         if file_name.endswith(extension):
             file_path = os.path.join(directory, file_name)
-            try:
-                os.remove(file_path)
-                print(f"Deleted file: {file_path}")
-            except Exception as e:
-                print(f"Error deleting file {file_path}: {e}")
+            os.remove(file_path)
 
-def predict(file,compression,signatures_complete="../signatures_complete"
-            ,output_dir="../compressed_together",compressed_files = "../compressed_files"):
-    append_files(file,signatures_complete,output_dir)
-    #compress appended files
-    for file_name in os.listdir(output_dir):
-        file_path = os.path.join(output_dir ,file_name)
-        zip_file(file_path,output_dir,compression)
-    delete_files_with_extension(output_dir,".freqs")
+def predict(file,compression,signatures_complete="../signature"
+            ,output_dir="../compressed_together",compressed_files = "../compressed"):
+    if os.path.exists(output_dir):
+        shutil.rmtree(output_dir)
     #signature of target
     file_path_obj = Path(file)
     file_name_without_extension = file_path_obj.stem
@@ -132,18 +134,27 @@ def predict(file,compression,signatures_complete="../signatures_complete"
     subprocess.run(command, check=True)
     file = file_name_without_extension+".freqs"
     
+    append_files(file,signatures_complete,output_dir)
+    #compress appended files
+    
+    for file_name in os.listdir(output_dir):
+        file_path = os.path.join(output_dir ,file_name)
+        zip_file(file_path,output_dir,compression)
+    delete_files_with_extension(output_dir,".freqs")
+    
     zip_file(file,".",compression)
     os.remove(file)
     file = file_name_without_extension+".zip"
-    print(file)
+
     
-  
+    
     sizes = dict()
     target_size = os.path.getsize(file)
     for file_name in os.listdir(output_dir):
         file_path = os.path.join(output_dir ,file_name)
         file_root, file_ext = os.path.splitext(file_name)
         sizes[file_root] = os.path.getsize(file_path)
+    
     # find max similarity
     min_sim = float('inf'),""
     sim_list = dict()
@@ -153,13 +164,24 @@ def predict(file,compression,signatures_complete="../signatures_complete"
         size = os.path.getsize(file_path)
         sim_list[file_root] = NCD(target_size,size,sizes[file_root])
         if sim_list[file_root] < min_sim[0]:
-            min_sim = (size,file_root)
-    print("Segment file:",file_name_without_extension)
-    print("prediction:",min_sim[1])
+            min_sim = (sim_list[file_root],file_root)
+    
+    for key, value in sim_list.items():
+        print(f"{key}: {value}")
+    print("Segmented file: ",file_name_without_extension)
+    print("Prediction: ",min_sim[1])
+    
+    
 def NCD(seg,complete,mix):
     return (mix - min(seg,complete))/max(seg,complete)  
-   
-
+def clean():
+    dirs = ["../compressed","../segment_of_music","../signature","../compressed_together"]   
+    for dir in dirs:
+        if os.path.exists(dir):
+            shutil.rmtree(dir)
+    for file_name in os.listdir("."):
+        if file_name != "run_seg.py":
+            os.remove(file_name)
            
 
 if __name__ == "__main__":
@@ -169,33 +191,31 @@ if __name__ == "__main__":
     # Subparser for the 'seg' command
     parser_seg = subparsers.add_parser('seg', help='Extract segment from audio files')
     parser_seg.add_argument('input_dir', type=str, help="Input directory containing audio files")
-    parser_seg.add_argument('output_dir', type=str, help="Output directory for processed audio files")
     parser_seg.add_argument('start_time', type=float, help="Start time for the segment to extract (in seconds)")
     parser_seg.add_argument('duration', type=float, help="Duration of the segment to extract (in seconds)")
     parser_seg.add_argument('-f', '--format', type=str, help="Output audio format (e.g., mp3, wav)")
     parser_seg.add_argument('-q', '--quiet', action='store_true', help="Suppress output messages")
 
     parser_sig = subparsers.add_parser('sig', help='Create music signature')
-    parser_sig.add_argument('input_dir', type=str, help="Input directory containing audio files")
-    parser_sig.add_argument('output_dir', type=str, help="Output directory for processed audio files")
     
     parser_sig = subparsers.add_parser('compress', help='compress database of complete musics')
-    parser_sig.add_argument('musics_database', type=str, help="musics database")
-    parser_sig.add_argument('out_compress', type=str, help="Output directory for the compressed musics")
     parser_sig.add_argument('compression', type=str, help="Compression method")
     
     parser_sig = subparsers.add_parser('pred', help='Predict to music given a segment')
-    parser_sig.add_argument('target_file', type=str, help="musics database")
+    parser_sig.add_argument('target_file', type=str, help="Segment of music to predict")
     parser_sig.add_argument('compression', type=str, help="Compression method")
     
+    parser_sig = subparsers.add_parser('clean', help='Clean all musics except database')
     args = parser.parse_args()
 
     if args.command == 'seg':
-        extract_segment(args.input_dir, args.output_dir, args.start_time, args.duration, args.format, args.quiet)
+        extract_segment( args.start_time, args.duration,args.input_dir, args.format, args.quiet)
     if args.command == 'sig':
-        create_signature(args.output_dir,args.input_dir)
+        create_signature()
     if args.command == 'compress':
-        compress_data_base(args.musics_database,args.out_compress,args.compression)
+        compress_data_base(args.compression)
     if args.command == "pred":
         predict(args.target_file,args.compression)
+    if args.command == "clean":
+        clean()
     
